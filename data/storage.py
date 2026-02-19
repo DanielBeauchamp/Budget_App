edb07fd1ccde4d92aed0e50d2060b1ff29c9f262
DanielBeauchamp <danbeauchamp07@gmail.com>
2026-02-19 09:30:46 -0700

    Update storage.py

--- data/storage.py
+++ data/storage.py
@@ -1,1 +1,26 @@
+import json
+import os
 
+DATA_FILE = "budget.json"
+
+
+def load_data():
+    if not os.path.exists(DATA_FILE):
+        return {
+            "period": "2026-02",
+            "expenses": [],
+            "limits": {
+                "Food": 150,
+                "Transportation": 75,
+                "Entertainment": 100,
+                "Misc": 50
+            }
+        }
+
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
+
+
+def save_data(data):
+    with open(DATA_FILE, "w") as file:
+        json.dump(data, file, indent=4)
